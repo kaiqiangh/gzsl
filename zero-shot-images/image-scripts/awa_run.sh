d073393146e049fd2b39dc07d63c74c5bf69a70d
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2020-12-14 19:43:46 +0000

    change config

--- zero-shot-images/image-scripts/awa_run.sh
+++ zero-shot-images/image-scripts/awa_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #sbatch --job-name=awa --gres=gpu:0 --mem=32768 --cpus-per-task=4 --output=output_awa.out awa_run.sh
+#sbatch --job-name=awa --gres=gpu:1 --mem=16384 --cpus-per-task=4 --output=output_awa.out awa_run.sh
 
 python run_awa_tfvaegan.py
