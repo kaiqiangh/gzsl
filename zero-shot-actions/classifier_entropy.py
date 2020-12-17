30f108442d65e5a11e02dbafba28e0b6d0e94f1d
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2020-12-17 00:55:06 +0000

    modify classifier cls_entropy

--- zero-shot-actions/classifier_entropy.py
+++ zero-shot-actions/classifier_entropy.py
@@ -162,11 +162,11 @@ class CLASSIFIER:
             entropy_batch = self.criterion(output, batch=True)
             # The following evaluation holds true as seen and unseen sets are validated separately.
             if seen_classes:
-                #pred = self.seen_cls_model(test_Xv)
-                pred = self.seen_cls_model['fc.weight'] * test_Xv + self.seen_cls_model['fc.bias']
+                pred = self.seen_cls_model(test_Xv)
+                #pred = self.seen_cls_model['fc.weight'] * test_Xv + self.seen_cls_model['fc.bias']
             else:
-                #pred = self.unseen_cls_model(test_Xv)
-                pred = self.seen_cls_model['fc.weight'] * test_Xv + self.unseen_cls_model['fc.bias']
+                pred = self.unseen_cls_model(test_Xv)
+                #pred = self.seen_cls_model['fc.weight'] * test_Xv + self.unseen_cls_model['fc.bias']
 
             _, pred = torch.max(pred.data, 1)
             entropy.extend(entropy_batch.data.view(-1).cpu().numpy())
