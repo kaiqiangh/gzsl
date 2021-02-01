22ff6dee6e4e583c4f7b6f768b6d600c8e9a75da
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-02-01 12:12:54 +0000

    inital exp.

--- zero-shot-actions/util.py
+++ zero-shot-actions/util.py
@@ -52,6 +52,9 @@ class DATA_LOADER(object):
             train_loc = matcontent['train_loc'].squeeze() - 1
             #val_unseen_loc = matcontent['val_loc'].squeeze() - 1
             #test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
+            trainval_loc = []
+            val_unseen_loc = []
+            test_seen_loc = []
             test_unseen_loc = matcontent['test_unseen_loc'].squeeze() - 1
 
             if opt.class_embedding == "att":
@@ -77,13 +80,10 @@ class DATA_LOADER(object):
             matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.dataset + "_semantics/" +
                                      "split_1/" + "att_splits.mat")
             # matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.class_embedding + "_splits.mat")
-            #trainval_loc = matcontent['trainval_loc'].squeeze() - 1
-            trainval_loc = []
+            trainval_loc = matcontent['trainval_loc'].squeeze() - 1
             train_loc = matcontent['train_loc'].squeeze() - 1
-            #val_unseen_loc = matcontent['val_loc'].squeeze() - 1
-            val_unseen_loc = []
-            #test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
-            test_seen_loc = []
+            val_unseen_loc = matcontent['val_loc'].squeeze() - 1
+            test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
             test_unseen_loc = matcontent['test_unseen_loc'].squeeze() - 1
 
             if opt.class_embedding == "wv":
