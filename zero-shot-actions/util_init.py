1bd75b8085b2caf164df4be466aab2c721c1d556
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-02-08 20:38:20 +0000

    3 object

--- zero-shot-actions/util_init.py
+++ zero-shot-actions/util_init.py
@@ -61,8 +61,8 @@ class DATA_LOADER(object):
                     print("with object semantics:")
                     self.attribute = torch.from_numpy(matcontent['att_all'].T).float()
                     # 1 object, 2 objects and 3 objects
-                    print("append 2 object.")
-                    self.attribute = self.attribute[:, :900]
+                    print("append 3 object.")
+                    #self.attribute = self.attribute[:, :900]
                     print(self.attribute.shape)
                     self.attribute /= self.attribute.pow(2).sum(1).sqrt().unsqueeze(1).expand(self.attribute.size(0),
                                                                                               self.attribute.size(1))
