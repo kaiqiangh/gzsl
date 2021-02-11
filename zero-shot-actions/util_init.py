c10203228103433e5394a12cb199926503c608e4
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-02-11 01:51:43 +0000

    10cls-exp11, append 3 obj

--- zero-shot-actions/util_init.py
+++ zero-shot-actions/util_init.py
@@ -59,7 +59,7 @@ class DATA_LOADER(object):
                 if opt.object:
                     # att_all: 300 + 900d
                     print("with object semantics:")
-                    # print("append 3 objects - 1200d")
+                    print("append 3 objects - 1200d")
                     self.attribute = torch.from_numpy(matcontent['att_all'].T).float()
                     # Different cases:
                     ################################################################################################
@@ -96,8 +96,8 @@ class DATA_LOADER(object):
                     #print("append 2 objects (1st + 3rd)")
                     #self.attribute = torch.hstack((self.attribute[:, :600], self.attribute[:, 900:]))
                     # 2nd + 3rd
-                    print("append 2 objects (2nd + 3rd)")
-                    self.attribute = torch.hstack((self.attribute[:, :300], self.attribute[:, 600:]))
+                    #print("append 2 objects (2nd + 3rd)")
+                    #self.attribute = torch.hstack((self.attribute[:, :300], self.attribute[:, 600:]))
 
                     ################################################################################################
 
