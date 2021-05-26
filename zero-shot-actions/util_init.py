1a9236edba37fa7e5b5cb60f8f868f1c21a6d0e2
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-05-26 23:09:13 +0100

    average

--- zero-shot-actions/util_init.py
+++ zero-shot-actions/util_init.py
@@ -134,12 +134,12 @@ class DATA_LOADER(object):
                     #self.attribute = (self.action + self.obj1 + self.obj2 + self.obj3) / 4
 
                     # Case: Average(1 object, 2 object, 3 object)
-                    print("Average(1 object, 2 object, 3 object)")
-                    self.attribute = (self.obj1 + self.obj2 + self.obj3) / 3
+                    #print("Average(1 object, 2 object, 3 object)")
+                    #self.attribute = (self.obj1 + self.obj2 + self.obj3) / 3
 
                     # Case: Average(Class, 1 object)
-                    #print("Average(Class, 1 object)")
-                    #self.attribute = (self.action + self.obj1) / 2
+                    print("Average(Class, 1 object)")
+                    self.attribute = (self.action + self.obj1) / 2
 
                     # Case: Average(Class, 2 object)
                     # print ("Average(Class, 2 object)")
